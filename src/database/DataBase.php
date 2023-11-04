f6c2e26dd6116f188c941c1972b3ec56a682b367
Rodrigo-363 <rodrigo.tecnologia@troupebrasil.com.br>
2023-11-04 13:56:01 -0300

    Implementação do Dotenv

--- src/database/DataBase.php
+++ src/database/DataBase.php
@@ -1,6 +1,8 @@
 <?php 
 namespace Database;
 
+use Dotenv\Dotenv;
+use Helpers\DD;
 use mysqli;
 
 abstract class DataBase extends mysqli
@@ -15,16 +17,26 @@ abstract class DataBase extends mysqli
 
     public function __construct()
     {
-        $this->host = "dbMysql";
+        $dotenv = Dotenv::createUnsafeImmutable("../")->load();
+        // $dotenv->load();
+        $this->host      = getenv('DB_HOST');
+        $this->user      = getenv('DB_USER');
+        $this->password  = getenv('DB_PASS');
+        $this->database  = getenv('DB_NAME');
+        $this->port      = getenv('DB_PORT');
+        $this->credentials = (object)['host' => $this->host, 'user' => $this->user, 'password' => $this->password, 'database' => $this->database, 'port' => $this->port];
+    }
+        /* $this->host = "dbMysql";
         $this->user = "root";
         $this->password = "root";
         $this->database = "toDoDatabase";
         $this->port = 3306;
         $this->credentials = (object)['host' => $this->host, 'user' => $this->user, 'password' => $this->password, 'database' => $this->database, 'port' => $this->port];
-    }
+     }*/
 
     protected function connection()
     {
+        // DD::dd($this->credentials);
         $this->conn = new mysqli($this->credentials->host, $this->credentials->user, $this->credentials->password, $this->credentials->database);
         $this->conn->set_charset("utf8mb4");
 
